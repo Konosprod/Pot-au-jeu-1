162b6ef01499b6185220d7fad9af0c5c9ba820ec
Konosprod <konosprod@free.fr>
2018-07-20 23:41:08 +0200

    Soon building scene
    Signed-off-by: Konosprod <konosprod@free.fr>

---
+++ Assets/Scripts/Gameplay/BodyPart.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PartType
+{
+    Arm,
+    Leg,
+    Body,
+    Head
+}
+
+public enum PartOrientation
+{
+    Left,
+    Right
+}
+
+public class BodyPart : MonoBehaviour {
+
+    public PartType partType;
+    public bool canPick;
+    public Sprite sprite;
+
+    //Leg Values
+    public float stamina;
+    public float movementSpeed;
+    public float sprintSpeed;
+    public PartOrientation legOrientation;
+
+    //Arm Values
+    public float damages;
+    public float range;
+    public PartOrientation armOrientation;
+
+    //Body Values
+    public float health;
+    public float armor;
+
+    //Head Values
+    public float intelligence;
+    public Skill skill;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public virtual void Animate()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/Builder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Builder : MonoBehaviour {
+
+    public GameObject listItems;
+    public GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        loadItems();
+    }
+
+    private void loadItems()
+    {
+        Inventory inventory = player.GetComponent<Inventory>();
+    }
+}
