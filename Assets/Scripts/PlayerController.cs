01c9a2a6bbf57489b4ef6e770fe81fb98302546d
WOLFROM Matthieu <keithdae@gmail.com>
2018-07-20 20:42:28 +0200

    Generic scripts
    Signed-off-by: WOLFROM Matthieu <keithdae@gmail.com>

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    
+    private Rigidbody2D rb;
+
+    private Vector2 speed = new Vector2(2, 2);
+    private Vector2 movement;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(inputX * speed.x, inputY * speed.y);		
+	}
+
+    void FixedUpdate()
+    {
+        rb.velocity = movement;
+    }
+}
