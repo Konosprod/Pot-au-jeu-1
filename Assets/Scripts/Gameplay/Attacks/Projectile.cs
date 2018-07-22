ed0b3df5f7e0962876caae6afb69d2b95bf44a99
WOLFROM Matthieu <keithdae@gmail.com>
2018-07-22 15:50:48 +0200

    Guaranted rooms before boss
    Signed-off-by: WOLFROM Matthieu <keithdae@gmail.com>

---
+++ Assets/Scripts/Gameplay/Attacks/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : Attack {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (childCol.IsPlayer())
+        {
+            if (Input.GetMouseButton(1))
+            {
+                if (canAttack)
+                {
+                    UseAttack();
+                }
+            }
+        }
+    }
+}
